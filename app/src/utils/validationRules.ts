010085fe754720a651a6ec65b766d18916191dd3
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2020-03-23 21:01:46 +0430

    translate validation messages to persian

--- app/src/utils/validationRules.ts
+++ app/src/utils/validationRules.ts
@@ -3,20 +3,23 @@ import persianRex from 'persian-rex';
 
 const id = Joi.number().integer().greater(0);
 
-const mobileNumber = Joi.string().pattern(/^9\d{9}$/, {name: 'mobileNumber'})
+const mobileNumber = Joi.string().pattern(/^9\d{9}$/, {name: 'شماره موبایل'})
     .length(10);
 
-const persianText = Joi.string().pattern(persianRex.text, {name: 'persianText'});
+const persianText = Joi.string().pattern(persianRex.text, {name: 'متن فارسی'});
 
-const persianLetter = Joi.string().pattern(persianRex.letter, { name: 'persianLetter'});
+const persianLetter = Joi.string().pattern(persianRex.letter, { name: 'حروف فارسی'});
 
 const persianLetterWithSpace = Joi.string()
-    .pattern(new RegExp('^(' + persianRex.lettersASCIRange + ')|(\s)+$'), { name: 'persianLetterWithSpace'});
+    .pattern(new RegExp('^(' + persianRex.lettersASCIRange + ')|(\s)+$'), { name: 'حروف فارسی و فاصله'});
+
+const englishNumber = Joi.string().pattern(/^[0-9]+$/, {name: 'اعداد انگلیسی'});
 
 export default {
     id,
     mobileNumber,
     persianText,
     persianLetter,
-    persianLetterWithSpace
+    persianLetterWithSpace,
+    englishNumber
 }
