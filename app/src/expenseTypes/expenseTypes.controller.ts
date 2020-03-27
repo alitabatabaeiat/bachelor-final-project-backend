e3aa57cd9663cd94afcc03bc66f8223cde958e61
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2020-03-27 17:40:55 +0430

    Bug fix

--- app/src/expenseTypes/expenseTypes.controller.ts
+++ app/src/expenseTypes/expenseTypes.controller.ts
@@ -13,14 +13,14 @@ const createExpenseType = async (req: Request, res: express.Response, next: expr
 const getAllExpenseTypes = async (req: Request, res: express.Response, next: express.NextFunction) => {
     const { user, body } = req;
     const expenseTypes = await service.getAllExpenseTypes(user, body);
-    res.status(201).jsend.success(expenseTypes);
+    res.jsend.success(expenseTypes);
 };
 
 const getExpenseType = async (req: Request, res: express.Response, next: express.NextFunction) => {
     const { user, body, params } = req;
     const extendedBody = _.assign(body, {id: params.id});
     const expenseTypes = await service.getExpenseType(user, extendedBody);
-    res.status(201).jsend.success(expenseTypes);
+    res.jsend.success(expenseTypes);
 };
 
 export default {
